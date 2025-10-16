97a924a36436a763c562ea3d0bc4f59c632023ca
Kevin Harrington <3482852+ke5C2Fin@users.noreply.github.com>
2025-10-16 11:35:19 -0600

    Refactor yaml_path.lua for improved readability

--- yaml_path.lua
+++ yaml_path.lua
@@ -3,67 +3,73 @@
 require('vis')
 
 local function check_line(line)
-	if line:match('^ *$') then return false end
-	if line:find('^ *#') then return false end
-	if line:find('^ *%*') then return false end
-	if line:find('^ *%&') then return false end
-	return true
+        if line:match('^ *$') then return false end
+        if line:find('^ *#') then return false end
+        if line:find('^ *%*') then return false end
+        if line:find('^ *%&') then return false end
+        return true
+end
+
+local function is_sequence(line)
+        return line:find('^ *%- ')
+end
+
+local set_seq_indicator(line)
+        if is_sequence(line) then
+                local seq_indicator = '[]'
+        else
+                local seq_indicator = ''
+        end
+        return seq_indicator
 end
 
 local function get_indent(line)
-	local indent = line:find('[^ ]')
-	local is_seq = false
-	if line:find('^ *%- ') then
-		is_seq = true
-		indent = indent + 2
-	end
-	return indent, is_seq
+        local indent = line:find('[^ ]')
+        if is_sequence(line) then
+                indent = indent + 2
+        end
+        return indent
 end
 
 local function get_yaml_key(line)
-	local key = line:match('^ *-? *([^ ]+):')
-	if not key then return false end
-	if key:find('.', 1, true) then
-	    key = string.format('"%s"', key)
-	end
-	return key
+        local key = line:match('^ *-? *([^ ]+):')
+        if not key then return false end
+        if key:find('.', 1, true) then
+                key = string.format('"%s"', key)
+        end
+        return key
 end
 
 vis:map(vis.modes.NORMAL, "<C-p>", function(keys)
-	local line_num = vis.win.selection.line
-	local curr_line = vis.win.file.lines[line_num]
-	if not check_line(curr_line) then return false end
+        local line_num = vis.win.selection.line
+        local curr_line = vis.win.file.lines[line_num]
+        if not check_line(curr_line) then return false end
 
-	local curr_indent, is_seq = get_indent(curr_line)
-	local trigger_indent = curr_indent
+        local curr_indent = get_indent(curr_line)
+        local trigger_indent = curr_indent
 
-	local yaml_key = get_yaml_key(curr_line)
-	if not yaml_key then return false end
-	local yaml_path = yaml_key
+        local yaml_key = get_yaml_key(curr_line)
+        if not yaml_key then return false end
+        local yaml_path = yaml_key
 
-	while curr_indent > 1 do
-		line_num = line_num - 1
-		if line_num > 0 then
-			curr_line = vis.win.file.lines[line_num]
-			if curr_line and check_line(curr_line) then
-				local seq_indicator = ''
-				if is_seq then
-					seq_indicator = '[]'
-				end
+        while curr_indent > 1 do
+                line_num = line_num - 1
+                if line_num > 0 then
+                        curr_line = vis.win.file.lines[line_num]
+                        if curr_line and check_line(curr_line) then
+                                curr_indent = get_indent(curr_line)
+                                if curr_indent < trigger_indent then
+                                        trigger_indent = curr_indent
+                                        local seq_indicator = set_seq_indicator(curr_line)
+                                        yaml_key = get_yaml_key(curr_line)
+                                        if yaml_key then
+                                                yaml_path = string.format('%s%s.%s', yaml_key, seq_indicator, yaml_path)
+                                        end
+                                end
+                        end
+                end
+        end
 
-				curr_indent, is_seq = get_indent(curr_line)
-				if curr_indent < trigger_indent then
-					trigger_indent = curr_indent
-
-					yaml_key = get_yaml_key(curr_line)
-					if yaml_key then
-						yaml_path = string.format('%s%s.%s', yaml_key, seq_indicator, yaml_path)
-					end
-				end
-			end
-		end
-	end
-
-	vis:info(yaml_path)
-	return yaml_path
+        vis:info(yaml_path)
+        return yaml_path
 end, "yaml path")
